3f983a5c3d2323f6cbb2791b1ce6ce3a501bf06a
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-04 14:42:35 +0200

    Start function to insert html checbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,32 @@
 // Il programma dovrà consentire di calcolare il prezzo del panino selezionando
 // o deselezionando gli ingredienti proposti.
 
+
+// Nested array with ingredients and prices
 var ingredients = [
-    ["cheese", '2.5']
-    ["eggs", '2']
-    ['mustard', '0.5']
-    ['tomato', '1']
-    ['lettuce', '1']
+    ["cheese", '2.5'],
+    ["eggs", '2'],
+    ['mustard', '0.5'],
+    ['tomato', '1'],
+    ['lettuce', '1'],
     ['ketchup', '0.5']
 ]
 
-function renderIngredients(el, price) {
-    var counter = 0;
-    while (counter < ingredients.length) {
 
+function renderIngredients(list) {
+    var counter = 0;
+    while (counter < list.length) {
+        document.querySelector('.ingredients').insertAdjacentHTML('beforeend', `
+        <img src="./assets/img/${counter[0]}.svg"
+        <div class="form-group">
+            <label for="${counter[0]}">${counter[0]}</label>
+            <input type="checkbox" name="${counter[0]}" id="${counter[0]}" data-price="${counter[1]}">
+        </div>
+        `)
+        counter++
+        console.log(counter[0]);
     }
+
 }
-var dataPrice = document.getAttribute('data-price')
-renderIngredients()
+
+renderIngredients(ingredients)
