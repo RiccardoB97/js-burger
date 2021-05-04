5b80ee3de3a81ba9b9b4c9008048c3c0acf3dc68
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-04 13:51:24 +0200

    Add basic structure html and css

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// Il programma dovrà consentire di calcolare il prezzo del panino selezionando
+// o deselezionando gli ingredienti proposti.
+
+var ingredients = [
+    ["cheese", '2.5']
+    ["eggs", '2']
+    ['mustard', '0.5']
+    ['tomato', '1']
+    ['lettuce', '1']
+    ['ketchup', '0.5']
+]
+
+function renderIngredients(el, price) {
+    var counter = 0;
+    while (counter < ingredients.length) {
+
+    }
+}
+var dataPrice = document.getAttribute('data-price')
+renderIngredients()
