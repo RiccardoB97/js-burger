a3fe1e1117f7a304e449b466472951afc600d1f6
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-04 15:59:52 +0200

    Fix function to insert ingredients in HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
 // Nested array with ingredients and prices
 var ingredients = [
     ["cheese", '2.5'],
-    ["eggs", '2'],
+    ["egg", '2'],
     ['mustard', '0.5'],
     ['tomato', '1'],
     ['lettuce', '1'],
@@ -13,20 +13,20 @@ var ingredients = [
 ]
 
 
-function renderIngredients(list) {
+function renderIngredients(list, el) {
     var counter = 0;
     while (counter < list.length) {
-        document.querySelector('.ingredients').insertAdjacentHTML('beforeend', `
-        <img src="./assets/img/${counter[0]}.svg"
+        el.insertAdjacentHTML('beforeend', `
+        <img src="./assets/img/${list[counter][0]}.svg"
         <div class="form-group">
-            <label for="${counter[0]}">${counter[0]}</label>
-            <input type="checkbox" name="${counter[0]}" id="${counter[0]}" data-price="${counter[1]}">
-        </div>
+            <label for="${list[counter][0]}">${list[counter][0]}</label>
+            <input type="checkbox" name="${list[counter][0]}" id="${list[counter][0]} " data-price="${list[counter][1]} ">
+        </div >
         `)
         counter++
         console.log(counter[0]);
     }
-
 }
 
-renderIngredients(ingredients)
+var ingredientsEl = document.querySelector('.ingredients');
+renderIngredients(ingredients, ingredientsEl)
